64322f3dc04571bf26c46bef6e5b782739b7b195
Lucca_Santos <lucca_santos@dell.com>
2020-04-13 22:35:43 -0300

    Adicionando arquivos iniciais.

---
+++ src/test/java/com/pucrs/qp/TriangulosTeste.java
@@ -0,0 +1,13 @@
+package com.pucrs.qp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TriangulosTeste {
+
+	@Test
+	public void helloWorld() {
+		assertTrue(true);
+	}
+}
